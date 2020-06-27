c17c51de77e4a1b17df84cac13b88b14d7bcc201
sunnepazzy123 <sunnepazzy123@gmail.com>
2020-06-27 02:43:40 -0700

    first commit

---
+++ src/views/Memories.vue
@@ -0,0 +1,252 @@
+<template>
+  <v-container fluid grid-list-xl>
+    <v-layout row text-center wrap>
+      <v-flex xs12>
+        <h1 class="display-3">Our Memories</h1>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/1.jpg')" aspect-ratio="1" ></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline text-xs-center">My Luv!!</h1><br>
+              <h2 class="subheading">Happy Birthday to You!!</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/5.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">My Favourite Pix</h1><br>
+              <h2 class="subheading">I always Luv hanging out with u..</h2>
+
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/34.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Best Smile Ever </h1><br>
+              <h2 class="pannel-body">I awlays trip when you smile...</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/25.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">SuperWoman!!</h1><br>
+              <h2 class="subheading">My Elegant Woman</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/32.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">NYSC ThrowBack</h1><br>
+              <h2 class="subheading">Platoon 5 Ever Green!!</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/11.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Best Roomie Ever🙄</h1><br>
+              <h2 class="subheading">@Seyi Thanks for the Cake 😂</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/53.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Nurse Seyi 🤩</h1><br>
+              <h2 class="subheading">Best Nurse of the Year 😣</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/41.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Twinnies!!</h1><br>
+              <h2 class="subheading">@Dhaamey,@FolorunsoPretty 😂🤣 </h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/54.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Ebony For Live </h1>
+              <h2 class="subheading"> My Celebrity Woman!! 😅</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/23.png')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">#Future will be like ....</h1><br>
+              <h2 class="subheading">My Best Geologist ...😶</h2><br>
+              <h2 class="subheading">Special Person.. 😂😂 </h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/51.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">ThrowBack Luv!!</h1><br>
+              <h2
+                class="subheading">You are always There.. 😂😅</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/14.jpg')" aspect-ratio="1" ></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">NYSC Reunion !!</h1><br>
+              <h2 class="subheading"
+              >Ladokite Reunion ?😜</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/333.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">WireDevTeam Queen</h1><br>
+              <h2
+                class="subheading"
+              >Thanks always and See you Soon😂</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/2.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">My Leo💦</h1><br>
+              <h2
+                class="subheading"
+              >Like Matrix u are to me..LOl 🤪</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/52.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Pre Birthday Pix🎂</h1><br>
+              <h2
+                class="subheading"
+              >Ruby on Rails 😂</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/55.jpg')" aspect-ratio="0.8"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">My Ruby On Rails</h1><br>
+              <h2
+                class="subheading"
+              >Gracias for been there 😂😂😂 </h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/43.jpg')" aspect-ratio="0.9"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">ThrowBack Baby 🎥🎬</h1>
+              <h2
+                class="subheading"
+              >You can remeber this ? Right🤔</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/img/3.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Leopard Spot</h1><br>
+              <h2 class="subheading">
+                Your Spot is glued to my Skin!!
+              </h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+
+      <v-flex xs12>
+        <v-img :src="require('../assets/bday-159.gif')" contain height="300"></v-img>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-btn to="/words" color="info">This is For You</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
